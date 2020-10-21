ed7ecfaf3b001e224b5d48cb76f88d6cf6057ed2
jutchn <61358893+jutchn@users.noreply.github.com>
2020-10-21 16:16:14 -0400

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,7 +1,8 @@
 #include "mymalloc.h"
 #include <stdio.h>
+#define MEMORY_SIZE 4096
 
-static unsigned char myblock[4096];
+static unsigned char myblock[MEMORY_SIZE];
 
 // cast size and inuse int into unsigned char | merge size and inuse into 2 bytes (16 bits) with 12 bits for size and 1 bit for inuse. 
 // inuse bit is placed at front and size at the end | place the 2 bytes directly into the unsigned char myblock array.
@@ -19,12 +20,10 @@ void numToChar(int size, int inuse, unsigned char *myblock){
     } else {
         myblock[0] = O0 | (size>>8);
     }
-    //printf("%d %d\n",myblock[0],myblock[1]);
 }
 
 int charToSize(unsigned char *both){
     unsigned char OF = '<'/4;
-    //printf("%d\n",(*(both) & OF))<<8;
     return (*(both) & OF)*256 + *(both + 1);
 }
 
@@ -40,62 +39,74 @@ void printMem(){
     int metaSize;
     int metaUse;  
     int index = 0;
-    while(index < 4096){
+    while(index < MEMORY_SIZE){
         
         metaSize = charToSize(myblock + index);
         metaUse = charToInUse(myblock + index);
-        //if(metaSize != 0){
-        printf("Metadata | Size: %d bytes | Use: %d\n", metaSize, metaUse);
-        //}
+
         if(metaSize==0){
             break;
         }
+        printf("Metadata: 2 bytes \nBlock Size: %d bytes \nTotal Block Size: %d \nIn Use: %s\n\n", metaSize, metaSize + 2, metaUse ? "True" : "False");
         index+=metaSize+2;
-        
     }
 }
-int mymalloc(int size, const char* file, int line){
+void* mymalloc(size_t size, const char* file, int line){
     if(size == 0){
-        return -1; //return a non null pointer?  
+        return myblock; //return a non null pointer?  
     }
     int metaSize;
     int metaUse;    
     int index = 0;
-    while(index < 4096){
+    while(index < MEMORY_SIZE){
         metaSize = charToSize(myblock + index);
         metaUse = charToInUse(myblock + index);
-        
+        // check current block for in use, 
+        // if not in use and malloced size fits in block
+        // set meta data to in use and resize
         if(metaUse == 0){
             if(size<=metaSize){
                 numToChar(size,1,myblock + index);
-                if(metaSize-size>2){
-                    numToChar(metaSize-size-2, 0, myblock + index + 2 + size);
+                // check if space before next block is big enough for malloc size
+                if(metaSize-size>=2 && index + 2 + size < MEMORY_SIZE - 2){
+                    //printf("%d %d\n", metaSize, index);
+                    numToChar(metaSize - size - 2, 0, myblock + index + 2 + size);
                 }
                 // or else donate
-                return index; //pointer to 
+                return myblock + index + 2; //pointer to 
             }
+        }
+        // if you exceed the bounds of memory
+        if(index + 2 + metaSize > MEMORY_SIZE){
+            //printf("%d %d\n", index, index + 2 + metaSize);
+            numToChar(MEMORY_SIZE-index-2, 0, myblock + index);
+            return myblock + index + 2;
         }
         index+=2;
         index+=metaSize;
     }
     
 
-    perror("error u fool");
-    return -1;
+    printf("error u fool\n");
+    return myblock;
 
 }
 
-int myfree(){
+void myfree(void* pointer, const char* file, int line){
 
 }
 
 int main(){
-    numToChar(4094, 0, myblock);
+    numToChar(MEMORY_SIZE - 2, 0, myblock);
+
     printMem();
     printf("\n");
     //printf("%d",myblock[5]);
-    mymalloc(3, "pee", 3);
-    mymalloc(4, "pee", 3);
-    mymalloc(5, "pee", 3);
+    //mymalloc(3, __FILE__, __LINE__);
+    //mymalloc(4, "pee", 3);
+    //mymalloc(5, "pee", 3);
+    //mymalloc(1000, "pee", 3);
+    mymalloc(1, "pee", 3);
+    //mymalloc(2, "pee", 3);
     printMem();
 }
