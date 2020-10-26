0ceedade2c78aecd249a034980d2893c2f7a01bb
JonathanWang123 <jonathan.wang830@gmail.com>
2020-10-25 20:13:08 -0400

    Add files via upload

--- memgrind.c
+++ memgrind.c
@@ -2,59 +2,72 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include "mymalloc.h"
-#define MEMORY_SIZE 4096
-
-int main(){
+int main() {
+    // Initialize timing variables
     struct timeval time;
-    double worktime[5] = {0};
+    double worktime[5] = { 0 };
     double start;
     double end;
     int trytime;
 
-    for(int w = 0; w < 50; w++){
-        // Workload #1
+    // Run 50 iterations of each workload and average their times
+    for (int w = 0; w < 50; w++) {
+        // Workload A: 
+        // malloc() 1 byte and free it 120 times
+
         // start timing
-        trytime = gettimeofday(&time, NULL);
-        if(trytime == -1) { printf("gettimeofday() failed"); } 
-        start = time.tv_sec + (double)time.tv_usec/1000000;
+        trytime = gettimeofday(&time, NULL); 
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        start = time.tv_sec + (double)time.tv_usec / 1000000;
+
         // malloc and free 120 times
         unsigned char* p;
-        for(int i = 0; i<120; i++){
+        for (int i = 0; i < 120; i++) {
             p = (unsigned char*)malloc(1);
             free(p);
         }
+
         //end timing
         trytime = gettimeofday(&time, NULL); 
-        if(trytime == -1) { printf("gettimeofday() failed"); } 
-        end = time.tv_sec + (double)time.tv_usec/1000000;
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        end = time.tv_sec + (double)time.tv_usec / 1000000;
         worktime[0] += (end - start);
-        
-        
-        // Workload #2
-        // start timing        
-        trytime = gettimeofday(&time, NULL);
-        if(trytime == -1) { printf("gettimeofday() failed"); }  
-        start = time.tv_sec + (double)time.tv_usec/1000000;
-        // malloc 120 times then free 120 times
+
+
+        // Workload B: 
+        // malloc() 1 byte, store pointer in array
+        // after 120 calls to malloc(), free all pointers
+
+        // start timing
+        trytime = gettimeofday(&time, NULL); 
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        start = time.tv_sec + (double)time.tv_usec / 1000000;
+
+        // malloc and store 120 times then free 120 times
         unsigned char* parray2[120];
-        for(int i = 0; i < 120; i++){
+        for (int i = 0; i < 120; i++) {
             parray2[i] = (unsigned char*)malloc(1);
         }
-        for(int i = 0; i < 120; i++){
+        for (int i = 0; i < 120; i++) {
             free(parray2[i]);
         }
+        
         //end timing
         trytime = gettimeofday(&time, NULL); 
-        if(trytime == -1) { printf("gettimeofday() failed"); } 
-        end = time.tv_sec + (double)time.tv_usec/1000000;
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        end = time.tv_sec + (double)time.tv_usec / 1000000;
         worktime[1] += end - start;
 
 
-        // Workload #3
+        // Workload C:  
+        // 240 times, randomly choose between a 1 byte malloc()
+        // or free()ing one of the malloc()ed pointers
+
         // start timing
         trytime = gettimeofday(&time, NULL);
-        if(trytime == -1) { printf("gettimeofday() failed"); }  
-        start = time.tv_sec + (double)time.tv_usec/1000000;
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        start = time.tv_sec + (double)time.tv_usec / 1000000;
+
         // do 120 mallocs and 120 frees randomly
         unsigned char* parray3[120];
         int nummalloc = 0;
@@ -62,96 +75,109 @@ int main(){
         int totalmalloc = 0;
         int freed;
 
-        void doMalloc(){
+        void doMalloc() {
             parray3[nummalloc] = (unsigned char*)malloc(1);
-            nummalloc++; 
+            nummalloc++;
             totalmalloc++;
         }
-        void doFree(){
+        void doFree() {
             freed = rand() % nummalloc;
             free(parray3[freed]);
-            parray3[freed] = parray3[nummalloc-1];
+            parray3[freed] = parray3[nummalloc - 1];
             nummalloc--;
         }
-        for(int i = 0; i < 240; i++){
-            if(nummalloc == 0){
+        for (int i = 0; i < 240; i++) {
+            if (nummalloc == 0) {
                 doMalloc();
-            } else if (totalmalloc == maxmalloc){
+            }
+            else if (totalmalloc == maxmalloc) {
                 doFree();
-            } else {
-                if(rand() % 2 == 1){
+            }
+            else {
+                if (rand() % 2 == 1) {
                     doMalloc();
-                } else {
+                }
+                else {
                     doFree();
                 }
             }
         }
+
         //end timing
-        trytime = gettimeofday(&time, NULL); 
-        if(trytime == -1) { printf("gettimeofday() failed"); } 
-        end = time.tv_sec + (double)time.tv_usec/1000000;
+        trytime = gettimeofday(&time, NULL);
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        end = time.tv_sec + (double)time.tv_usec / 1000000;
         worktime[2] += end - start;
 
 
-        // Workload #4
+        // Workload D: 
+        // Fill memory with 2 byte blocks and free every other block
+        // Then malloc into those spaces to fill memory again and free everything
+
         // start timing
         trytime = gettimeofday(&time, NULL);
-        if(trytime == -1) { printf("gettimeofday() failed"); }
-        start = time.tv_sec + (double)time.tv_usec/1000000;
-        // fill up memory with 2 byte mallocs and free every other malloced block, then malloc into those spaces to fill up memory again and free
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        start = time.tv_sec + (double)time.tv_usec / 1000000;
+
         int iterations = 1024;
         unsigned char* parray4[iterations];
-        for(int i = 0; i < iterations; i++){
+        for (int i = 0; i < iterations; i++) {
             parray4[i] = (unsigned char*)malloc(2);
         }
-        for(int i = 0; i < iterations/2; i+=2){
+        for (int i = 0; i < iterations / 2; i += 2) {
             free(parray4[i]);
         }
-        for(int i = 0; i < iterations/2; i+=2){
+        for (int i = 0; i < iterations / 2; i += 2) {
             parray4[i] = (unsigned char*)malloc(2);
         }
-        for(int i = 0; i<iterations; i++){
+        for (int i = 0; i < iterations; i++) {
             free(parray4[i]);
         }
+
         // end timing
-        trytime = gettimeofday(&time, NULL); 
-        if(trytime == -1) { printf("gettimeofday() failed"); } 
-        end = time.tv_sec + (double)time.tv_usec/1000000;
+        trytime = gettimeofday(&time, NULL);
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        end = time.tv_sec + (double)time.tv_usec / 1000000;
         worktime[3] += end - start;
 
-        
-        // Workload #5
+
+        // Workload E: 
+        // malloc 2 bytes 1020 times, free blocks that aren't multiples of 5 in 1,2,4,3 order
+        // Finally malloc 14 bytes to fill up memory and free everything
+
         // start timing
         trytime = gettimeofday(&time, NULL);
-        if(trytime == -1) { printf("gettimeofday() failed"); }  
-        start = time.tv_sec + (double)time.tv_usec/1000000;
-        // malloc 2 bytes 1020 times, free blocks that aren't multiples of 5 in 1,2,4,3 order to demonstrate merging property of metadata, malloc 14 bytes to fill up memory and free
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        start = time.tv_sec + (double)time.tv_usec / 1000000;
+
         iterations = 1020;
         unsigned char* parray5[iterations];
-        for(int i = 0; i < iterations; i++){
+        for (int i = 0; i < iterations; i++) {
             parray5[i] = (unsigned char*)malloc(2);
         }
-        for(int i = 0; i < iterations; i+=5){
-            free(parray5[i+1]);
-            free(parray5[i+2]);
-            free(parray5[i+4]);
-            free(parray5[i+3]);
+        for (int i = 0; i < iterations; i += 5) {
+            free(parray5[i + 1]);
+            free(parray5[i + 2]);
+            free(parray5[i + 4]);
+            free(parray5[i + 3]);
         }
-        for(int i = 0; i < iterations; i+=5){
-            parray5[i+1] = (unsigned char*)malloc(14);
+        for (int i = 0; i < iterations; i += 5) {
+            parray5[i + 1] = (unsigned char*)malloc(14);
         }
-        for(int i = 0; i<iterations; i+=5){
+        for (int i = 0; i < iterations; i += 5) {
             free(parray5[i]);
-            free(parray5[i+1]);
+            free(parray5[i + 1]);
         }
+
         // end timing
-        trytime = gettimeofday(&time, NULL); 
-        if(trytime == -1) { printf("gettimeofday() failed"); } 
-        end = time.tv_sec + (double)time.tv_usec/1000000;
+        trytime = gettimeofday(&time, NULL);
+        if (trytime == -1) { printf("gettimeofday() failed"); }
+        end = time.tv_sec + (double)time.tv_usec / 1000000;
         worktime[4] += end - start;
     }
-    for(int j = 0; j<5; j++){
-        worktime[j] = worktime[j]/50;
-        printf("Mean runtime for workload %d: %.20lf\n",j+1,worktime[j]);
-    }   
+
+    for (int j = 0; j < 5; j++) {
+        worktime[j] = worktime[j] / 50;
+        printf("Mean runtime for workload %d: %.15lf seconds | %.5lf microseconds\n", j + 1, worktime[j], worktime[j] * 1000000);
+    }
 }
--- mymalloc.c
+++ mymalloc.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stddef.h>
-#define MEMORY_SIZE 4096
+#define MEMORY_SIZE 4096 // Global constant representing total memory size in bytes
+#define METADATA_SIZE 2 // Global Constant representing metadata size in bytes 
 
-static unsigned char myblock[MEMORY_SIZE];
+static unsigned char myblock[MEMORY_SIZE]; // Global char array to represent memory heap
 
-// cast size and inuse int into unsigned char | merge size and inuse into 2 bytes (16 bits) with 12 bits for size and 1 bit for inuse. 
-// inuse bit is placed at front and size at the end | place the 2 bytes directly into the unsigned char currBlock array.
-void numToChar(int size, int inuse, unsigned char* currBlock) {
+// makeMetadata() turns size and inuse values into metadata and places it into memory
+// Metadata is formatted in 2 bytes with the following structure: x000 yyyy yyyy yyyy, where x is the InUse bool and y bits are the size
+// This allows for a max MEMORY_SIZE of 4096 but it could be expanded to 3 bytes of the form: x000 0000 yyyy yyyy yyyy yyyy if it needs to be scaled
+void makeMetadata(int size, int inuse, unsigned char* currBlock) {
+    // cast size and inuse int into unsigned char and place the 2 bytes directly into the unsigned char currBlock array.
     unsigned char FF = ('-' / 3) * ('"' / 2);       // hex: FF | binary: 1111 1111
     unsigned char eight0 = '@' * 2;               // hex: 80 | binary: 1000 0000
     unsigned char O0 = 'a' * 0;                   // hex: 00 | binary: 0000 0000
@@ -23,12 +26,14 @@ void numToChar(int size, int inuse, unsigned char* currBlock) {
     }
 }
 
-int charToSize(unsigned char* both) {
+// Takes a pointer to a memory block's metadata and returns its Size value
+int metaToSize(unsigned char* both) {
     unsigned char OF = '<' / 4;
     return (*(both)&OF) * 256 + *(both + 1);
 }
 
-int charToInUse(unsigned char* first) {
+// Takes a pointer to a memory block's metadata and returns its InUse boolean
+int metaToInUse(unsigned char* first) {
     if (*(first) >> 7 == 1) {
         return 1;
     }
@@ -37,114 +42,134 @@ int charToInUse(unsigned char* first) {
     }
 }
 
+// Used for testing. Function goes through memory and prints each block's metadata
 void printMem() {
-    
     int metaSize;
     int metaUse;
     int index = 0;
     while (index < MEMORY_SIZE) {
 
-        metaSize = charToSize(myblock + index);
-        metaUse = charToInUse(myblock + index);
+        metaSize = metaToSize(myblock + index);
+        metaUse = metaToInUse(myblock + index);
 
         if (metaSize == 0) {
             break;
         }
         printf("Metadata: 2 bytes \nBlock Size: %d bytes \nTotal Block Size: %d \nIn Use: %s\n\n", metaSize, metaSize + 2, metaUse ? "True" : "False");
-        index += metaSize + 2;
+        index += metaSize + METADATA_SIZE;
     }
     printf("---------------------------------------------\n");
 }
 
+// Takes a size variable and allocates a block of memory of that size 
+// along with 2 bytes of meta data that describe the block 
+// Returns a pointer to the allocated block of memory
 void* mymalloc(size_t size, const char* file, int line) {
     // initialize memory with metadata of block size 4094 and inuse 0
-    if(charToSize(myblock + 0) == 0){
-        numToChar(MEMORY_SIZE - 2, 0, myblock);
+    if (metaToSize(myblock + 0) == 0) {
+        makeMetadata(MEMORY_SIZE - METADATA_SIZE, 0, myblock);
     }
     if (size == 0) {
-        return myblock; //return a non null pointer?  
+        return NULL; //If size is 0 return a null pointer  
     }
     int metaSize;
     int metaUse;
     int index = 0;
     while (index < MEMORY_SIZE) {
-        metaSize = charToSize(myblock + index);
-        metaUse = charToInUse(myblock + index);
+        metaSize = metaToSize(myblock + index);
+        metaUse = metaToInUse(myblock + index);
         // check current block for in use, 
         // if not in use and malloced size fits in block
         // set meta data to in use and resize
         if (metaUse == 0) {
             if (size <= metaSize) {
-                numToChar(size, 1, myblock + index);
+                makeMetadata(size, 1, myblock + index);
                 // check if space before next block is big enough for malloc size
-                if (metaSize - size >= 2 && index + 2 + size < MEMORY_SIZE - 2) {
-                    numToChar(metaSize - size - 2, 0, myblock + index + 2 + size);
+                if (metaSize - size >= METADATA_SIZE && index + METADATA_SIZE + size < MEMORY_SIZE - METADATA_SIZE) {
+                    makeMetadata(metaSize - size - METADATA_SIZE, 0, myblock + index + METADATA_SIZE + size);
                 }
                 // or else donate
-                return myblock + index + 2; //pointer to 
+                return myblock + index + METADATA_SIZE; //pointer to 
             }
         }
         // if you exceed the bounds of memory
-        if (index + 2 + metaSize > MEMORY_SIZE) {
-            numToChar(MEMORY_SIZE - index - 2, 0, myblock + index);
-            return myblock + index + 2;
+        if (index + METADATA_SIZE + metaSize > MEMORY_SIZE) {
+            makeMetadata(MEMORY_SIZE - index - METADATA_SIZE, 0, myblock + index);
+            return myblock + index + METADATA_SIZE;
         }
-        index += 2;
+        index += METADATA_SIZE;
         index += metaSize;
     }
 
-
-    printf("Malloc Error: No available space to allocate (file:%s line:%d)\n",file,line);
-    return myblock;
-
+    // If no space is available to allocate a block of size bytes, throw an error and return a null pointer
+    printf("\033[1;31m");
+    printf("Malloc Error: No available space to allocate (file:%s line:%d)\n", file, line);
+    printf("\033[0m");
+    return NULL;
 }
 
+// Takes a pointer and free the block of memory it points to, if it points to any
 void myfree(void* pointer, const char* file, int line) {
-    int index = 0;
-    int isInHeap = 0;
-    while (index < MEMORY_SIZE) {
-        unsigned char* currPointer = myblock + index;
-        if ((unsigned char*)pointer == currPointer+2) {
-            if (charToInUse(currPointer)) {
-                numToChar(charToSize(currPointer), 0, currPointer);
-            }
-            else {
-               printf("Free Error: Memory Region Not In Use (file:%s line:%d)\n",file,line);
-               return;
-            }
-            isInHeap = 1;
-            break;
-        }
-        index += charToSize(currPointer) + 2;
-    }
-    if (!isInHeap) {
-        printf("\033[0; 31m");
-        printf("Free Error: Variable Not in Memory (file:%s line:%d)\n",file,line);
+
+    // Throw null pointer error
+    if (pointer == NULL) {
+        printf("\033[1;31m");
+        printf("Free Error: Null Pointer (file:%s line:%d)\n", file, line);
         printf("\033[0m");
         return;
     }
 
-    unsigned char* prevBlock = myblock;
-    int prevSize = charToSize(prevBlock);
-    int prevUse = charToInUse(prevBlock);
-    index = prevSize+2;
+    // Loop through memory until you either find the block or reach the end of memory
+    int index = 0;
+    int isInHeap = 0; // Boolean to represent if pointer points to block of memory
     while (index < MEMORY_SIZE) {
-        if (prevSize == 0) {
+        unsigned char* currPointer = myblock + index;
+        if ((unsigned char*)pointer == currPointer + METADATA_SIZE) {
+            // Set InUse boolean to false if pointer is in memory
+            if (metaToInUse(currPointer)) {
+                makeMetadata(metaToSize(currPointer), 0, currPointer);
+            }
+            else {
+                // Throw error if block being pointed to is not in use
+                printf("\033[1;31m");
+                printf("Free Error: Memory Region Not In Use (file:%s line:%d)\n", file, line);
+                printf("\033[0m");
+                return;
+            }
+            isInHeap = 1;
             break;
         }
+        index += metaToSize(currPointer) + METADATA_SIZE;
+    }
+    if (!isInHeap) {
+        // Throw error if pointer doesn't point to block of memory
+        printf("\033[1;31m");
+        printf("Free Error: Variable Not in Memory (file:%s line:%d)\n", file, line);
+        printf("\033[0m");
+        return;
+    }
+
+    // Loop through memory array and look for adjacent unused blocks
+    unsigned char* prevBlock = myblock;
+    int prevSize = metaToSize(prevBlock);
+    int prevUse = metaToInUse(prevBlock);
+    index = prevSize + METADATA_SIZE;
+    while (index < MEMORY_SIZE) {
         unsigned char* currBlock = myblock + index;
-        int currSize = charToSize(currBlock);
-        int currUse = charToInUse(currBlock);
-        
+        int currSize = metaToSize(currBlock);
+        int currUse = metaToInUse(currBlock);
+
         if (prevUse == 0 && currUse == 0) {
-            numToChar(prevSize + currSize + 2,0,prevBlock);
-            prevSize = prevSize + currSize + 2;
+            // Combine unused blocks into one block
+            makeMetadata(prevSize + currSize + METADATA_SIZE, 0, prevBlock);
+            prevSize = prevSize + currSize + METADATA_SIZE;
         }
         else {
             prevSize = currSize;
             prevUse = currUse;
             prevBlock = currBlock;
         }
-        index += currSize + 2;
+        index += currSize + METADATA_SIZE;
     }
+    return;
 }
